c476bb1a967653b0bc9ca025ddf1b4cb6ceed840
Wietse Neven <hello@wietseneven.com>
2017-05-12 17:06:15 -0500

    The navigation items now have a background color when active

--- src/base.js
+++ src/base.js
@@ -68,228 +68,239 @@ export default class Base extends Component {
     const { halfWidth, currentPage, menuItems, smallHeader, width } = this.state;
     let menuItem = false;
 
-    if (width < 992) return;
-    this.pages.map((page) => {
-      const offsetLeft = page.base.getBoundingClientRect().left;
-      if (page.props.in_menu) menuItem = page.props.id;
+    if (width < 992) {
 
-      if (offsetLeft > -halfWidth && offsetLeft < halfWidth) {
-        const current = page.props.id;
-        if (current === menuItem) menuItem = current;
-        const newItems = [];
-        // console.log(page);
-        if (currentPage !== current) {
-          menuItems.map((item) => {
-            item.current = item.id === menuItem;
-            newItems.push(item);
-          });
+    } else {
+      this.pages.map((page) => {
+        const offsetLeft = page.base.getBoundingClientRect().left;
+        if (page.props.in_menu) menuItem = page.props.id;
 
-          if (current > 1 && !smallHeader) {
-            this.updateHeader();
-          } else if (current <= 1 && smallHeader) {
-            this.updateHeader();
+        if (offsetLeft > -halfWidth && offsetLeft < halfWidth) {
+          const current = page.props.id;
+          if (current === menuItem) menuItem = current;
+          const newItems = [];
+          // console.log(page);
+          if (currentPage !== current) {
+            menuItems.map((item) => {
+              item.current = item.id === menuItem;
+              newItems.push(item);
+            });
+
+            if (current > 1 && !smallHeader) {
+              this.updateHeader();
+            } else if (current <= 1 && smallHeader) {
+              this.updateHeader();
+            }
+            this.setState({ currentPage: current, menuItems: newItems });
           }
-          this.setState({ currentPage: current, menuItems: newItems });
+        }
+      })
+    }
+  }
+
+    updateHeader()
+    {
+      const { smallHeader } = this.state;
+      const headerElement = document.querySelector('#header');
+      if (!headerElement) return;
+      this.setState({ smallHeader: !smallHeader });
+      if (smallHeader) {
+        headerElement.classList.remove('header--compact');
+      } else {
+        headerElement.classList.add('header--compact');
+      }
+
+      const interval = setInterval(() => {
+        this.updateDimensions()
+      }, 10);
+      setTimeout(() => {
+        clearInterval(interval);
+      }, 550);
+    }
+
+    updateDimensions()
+    {
+      const { smallHeader } = this.state;
+      const header = document.querySelector('#header');
+      const navbarCollapse = document.querySelector('#navbarCollapse');
+      const windowHeight = window.innerHeight;
+      const windowWidth = window.innerWidth;
+      const headerHeight = (smallHeader) ? ((navbarCollapse) ? navbarCollapse.offsetHeight : 0) : ((header) ? header.offsetHeight : 0);
+      const interactiveHeight = windowHeight - headerHeight;
+      this.setState({ height: interactiveHeight, width: windowWidth, halfWidth: windowWidth / 2 });
+    }
+
+    setData()
+    {
+      let dataExists = true;
+      let interactiveData;
+      let dataUri;
+      try {
+        if (transition_in_bojaya_data) {
+          dataExists = true;
+          interactiveData = transition_in_bojaya_data;
+        }
+      } catch (e) {
+        dataExists = false;
+      }
+
+      if (!dataExists) {
+        this.setState({ data: data });
+      } else {
+        if (interactiveData.dataUri) {
+          dataUri = interactiveData.dataUri;
+          this.fetchData(dataUri);
+        } else {
+          this.formatData(data);
         }
       }
-    })
-  }
-
-  updateHeader() {
-    const { smallHeader } = this.state;
-    const headerElement = document.querySelector('#header');
-    if (!headerElement) return;
-    this.setState({ smallHeader: !smallHeader });
-    if (smallHeader) {
-      headerElement.classList.remove('header--compact');
-    } else {
-      headerElement.classList.add('header--compact');
     }
 
-    const interval = setInterval(() => {
-      this.updateDimensions()
-    }, 10);
-    setTimeout(() => {
-      clearInterval(interval);
-    }, 550);
-  }
-
-  updateDimensions() {
-    const { smallHeader } = this.state;
-    const header = document.querySelector('#header');
-    const navbarCollapse = document.querySelector('#navbarCollapse');
-    const windowHeight = window.innerHeight;
-    const windowWidth = window.innerWidth;
-    const headerHeight = (smallHeader) ? ((navbarCollapse) ? navbarCollapse.offsetHeight : 0) : ((header) ? header.offsetHeight : 0);
-    const interactiveHeight = windowHeight - headerHeight;
-    this.setState({ height: interactiveHeight, width: windowWidth, halfWidth: windowWidth / 2 });
-  }
-
-  setData() {
-    let dataExists = true;
-    let interactiveData;
-    let dataUri;
-    try {
-      if (transition_in_bojaya_data) {
-        dataExists = true;
-        interactiveData = transition_in_bojaya_data;
-      }
-    } catch (e) {
-      dataExists = false;
+    fetchData(uri)
+    {
+      fetch(uri)
+        .then((response) => {
+          return response.json()
+        }).then((json) => {
+        this.formatData(json);
+        console.log('Data fetched');
+      }).catch((ex) => {
+        console.log('parsing failed', ex)
+      })
     }
 
-    if (!dataExists) {
-      this.setState({ data: data });
-    } else {
-      if (interactiveData.dataUri) {
-        dataUri = interactiveData.dataUri;
-        this.fetchData(dataUri);
-      } else {
-        this.formatData(data);
-      }
+    formatData(rawData)
+    {
+      const menuItems = [];
+      const pages = [];
+
+      rawData.map((rawItem) => {
+        const item = {
+          id: rawItem.id,
+          hidden: rawItem.hidden,
+          in_menu: rawItem.in_menu,
+          title: rawItem.title,
+          subtitle: rawItem.subtitle,
+          intro: rawItem.intro,
+          content: rawItem.content,
+          page_type: rawItem.page_type,
+          background_image: rawItem.background_image
+        };
+
+        if (!item.hidden) pages.push(item);
+        if (!item.hidden && item.in_menu) menuItems.push(item);
+      });
+
+      this.setState({ pages, menuItems });
     }
-  }
 
-  fetchData(uri) {
-    fetch(uri)
-      .then((response) => {
-        return response.json()
-      }).then((json) => {
-      this.formatData(json);
-      console.log('Data fetched');
-    }).catch((ex) => {
-      console.log('parsing failed', ex)
-    })
-  }
+    getPages()
+    {
+      const { pages, width } = this.state;
+      return pages.map((page, index) => {
+        const { id, page_type } = page;
 
-  formatData(rawData) {
-    const menuItems = [];
-    const pages = [];
+        let prev, next;
+        if (pages[index - 1]) {
+          // console.log(pages[index - 1].title, page.title);
+          prev = pages[index - 1].id;
+        }
+        if (pages[index + 1]) {
+          next = pages[index + 1].id;
+        }
 
-    rawData.map((rawItem) => {
-      const item = {
-        id: rawItem.id,
-        hidden: rawItem.hidden,
-        in_menu: rawItem.in_menu,
-        title: rawItem.title,
-        subtitle: rawItem.subtitle,
-        intro: rawItem.intro,
-        content: rawItem.content,
-        page_type: rawItem.page_type,
-        background_image: rawItem.background_image
-      };
-
-      if (!item.hidden) pages.push(item);
-      if (!item.hidden && item.in_menu) menuItems.push(item);
-    });
-
-    this.setState({ pages, menuItems });
-  }
-
-  getPages() {
-    const { pages, width } = this.state;
-    return pages.map((page, index) => {
-      const { id, page_type } = page;
-
-      let prev, next;
-      if (pages[index - 1]) {
-        // console.log(pages[index - 1].title, page.title);
-        prev = pages[index - 1].id;
-      }
-      if (pages[index + 1]) {
-        next = pages[index + 1].id;
-      }
-
-      let arrowLeft, arrowRight;
-      if (width > 992) {
-        arrowLeft = (
-          <Arrow to={prev} direction="prev" callback={this.goToPage} />
-        );
-        arrowRight = (
-          <Arrow to={next} direction="next" callback={this.goToPage} />
-        );
-      }
-
-      switch (page_type) {
-        case 'main_intro':
-          return (
-            <MainIntro ref={(ref) => this.pages[index] = ref} {...page} key={id}>
-              {arrowLeft}{arrowRight}
-            </MainIntro>
+        let arrowLeft, arrowRight;
+        if (width > 992) {
+          arrowLeft = (
+            <Arrow to={prev} direction="prev" callback={this.goToPage} />
           );
-          break;
-        case 'chapter_intro':
-          return (
-            <ChapterIntro ref={(ref) => this.pages[index] = ref} {...page} key={id}>
-              {arrowLeft}{arrowRight}
-            </ChapterIntro>
+          arrowRight = (
+            <Arrow to={next} direction="next" callback={this.goToPage} />
           );
-          break;
-        case 'text':
-          return (
-            <Text ref={(ref) => this.pages[index] = ref} {...page} key={id}>
-              {arrowLeft}{arrowRight}
-            </Text>
-          )
+        }
+
+        switch (page_type) {
+          case 'main_intro':
+            return (
+              <MainIntro ref={(ref) => this.pages[index] = ref} {...page} key={id}>
+                {arrowLeft}{arrowRight}
+              </MainIntro>
+            );
+            break;
+          case 'chapter_intro':
+            return (
+              <ChapterIntro ref={(ref) => this.pages[index] = ref} {...page} key={id}>
+                {arrowLeft}{arrowRight}
+              </ChapterIntro>
+            );
+            break;
+          case 'text':
+            return (
+              <Text ref={(ref) => this.pages[index] = ref} {...page} key={id}>
+                {arrowLeft}{arrowRight}
+              </Text>
+            )
+        }
+      });
+    }
+
+    goToPage(id)
+    {
+      const { width } = this.state;
+      let pageRef;
+      this.pages.map((page) => {
+        if (page.props.id === id) pageRef = page;
+      });
+
+      if (pageRef) {
+        const element = pageRef.base;
+
+        if (width > 992) {
+          const offset = element.offsetLeft;
+
+          this.pagesRoot.classList.add(s.opacity0);
+          setTimeout(() => {
+            this.pagesRoot.scrollLeft = offset;
+            this.checkActivePage();
+          }, 500);
+          setTimeout(() => {
+            this.pagesRoot.classList.remove(s.opacity0);
+          }, 500);
+        } else {
+          const header = document.querySelector('#header');
+          const headerHeight = (header) ? header.offsetHeight : 0;
+          const offset = element.offsetTop + headerHeight;
+          this.pagesRoot.classList.add(s.opacity0);
+          setTimeout(() => {
+            document.body.scrollTop = offset;
+            this.checkActivePage();
+          }, 500);
+          setTimeout(() => {
+            this.pagesRoot.classList.remove(s.opacity0);
+          }, 500);
+        }
       }
-    });
-  }
+    }
 
-  goToPage(id) {
-    const { width } = this.state;
-    let pageRef;
-    this.pages.map((page) => {
-      if (page.props.id === id) pageRef = page;
-    });
+    render(props, state)
+    {
+      const { height, width, menuItems } = state;
+      const pages = this.getPages();
 
-    if (pageRef) {
-      const element = pageRef.base;
-
+      let style;
       if (width > 992) {
-        const offset = element.offsetLeft;
-
-        this.pagesRoot.classList.add(s.opacity0);
-        setTimeout(() => {
-          this.pagesRoot.scrollLeft = offset;
-          this.checkActivePage();
-        }, 500);
-        setTimeout(() => {
-          this.pagesRoot.classList.remove(s.opacity0);
-        }, 500);
-      } else {
-        const header = document.querySelector('#header');
-        const headerHeight = (header) ? header.offsetHeight : 0;
-        const offset = element.offsetTop + headerHeight;
-        this.pagesRoot.classList.add(s.opacity0);
-        setTimeout(() => {
-          document.body.scrollTop = offset;
-          this.checkActivePage();
-        }, 500);
-        setTimeout(() => {
-          this.pagesRoot.classList.remove(s.opacity0);
-        }, 500);
+        style = {
+          height
+        };
       }
-    }
-  }
 
-  render(props, state) {
-    const { height, width, menuItems } = state;
-    const pages = this.getPages();
-
-    let style;
-    if (width > 992) {
-      style = {
-        height
-      };
-    }
-
-    return (
-      <div className={s.container} style={style}>
-        <div ref={(ref) => this.pagesRoot = ref} onWheel={this.scroll} className={s.pages}>
-          {pages}
+      return (
+        <div className={s.container} style={style}>
+          <div ref={(ref) => this.pagesRoot = ref} onWheel={this.scroll} className={s.pages}>
+            {pages}
+          </div>
+          <ChapterMenu items={menuItems} openMenuItem={this.goToPage} />
         </div>
-        <ChapterMenu items={menuItems} openMenuItem={this.goToPage} />
-      </div>
-    )
+      )
+    }
   }
-}
