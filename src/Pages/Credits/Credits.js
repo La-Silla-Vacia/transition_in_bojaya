54cc9f040eb5cbb0dba01c90508c4ece3902819e
Wietse Neven <hello@wietseneven.com>
2017-05-13 14:14:40 -0500

    New credits

--- src/Pages/Credits/Credits.js
+++ src/Pages/Credits/Credits.js
@@ -20,19 +20,45 @@ export default class ChapterIntro extends Component {
               <table className={s.table}>
                 <tbody>
                 <tr>
-                  <td>Historia:</td>
-                  <td>Natalia Arenas</td>
+                  <td>
+                    Texto:
+                  </td>
+                  <td>
+                    Natalia Arenas
+                  </td>
                 </tr>
                 <tr>
-                  <td>Fotos:</td>
-                  <td>Nombre</td>
+                  <td>
+                    Fotografías:
+                  </td>
+                  <td>
+                    Rafael Espinosa Murcia, Unidad para la Atención y Reparación Integral a las Víctimas<br />
+                    Lina Jaramillo, Ceaf, Universidad Icesi <br />
+                    Jóvenes participantes del Taller de Periodismo que viene realizando en la Institución Educativa
+                    César Contó, de Bellavista, la Maestría en Periodismo de la Universidad Icesi. <br />
+                    Natalia Arenas
+                  </td>
                 </tr>
                 <tr>
-                  <td>Diseño y desarrollo:</td>
-                  <td>Wietse Neven</td>
+                  <td>
+                    Música:
+                  </td>
+                  <td>
+                    “Decimoquinto aniversario”. Grupo de Cantadoras de Pogue; Universidad Icesi; Pacifico Records y
+                    Fundación Ford.
+                  </td>
+                </tr>
+                <tr>
+                  <td width="162">
+                    Diseño y desarrollo:
+                  </td>
+                  <td>
+                    Wietse Neven
+                  </td>
                 </tr>
                 </tbody>
               </table>
+              <small className={s.foottext}>Especial agradecimiento al Centro de Estudios Afrodiaspóricos, Ceaf, de la Universidad Icesi, Cali.</small>
             </div>
           </div>
         </div>
